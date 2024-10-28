613e22a35877454ef8698167251c0b1c83233f7b
NebiyouChanie <cnebiyu@gmail.com>
2024-10-28 20:28:23 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./aboutus.html",
+    "./index.html",
+    "./locations.html",
+    "./menu.html",
+    "./locations/location1.html",
+    "./locations/location2.html",
+    "./locations/location3.html",
+    "./menu/menuitem1.html",
+    "./menu/menuitem2.html",
+    "./menu/menuitem3.html",
+    "./menu/menuitem4.html",
+    "./special/special1.html",
+    "./special/special2.html",
+    "./special/special3.html",
+     
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
